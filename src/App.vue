26ebbc580d2d6361a42eb031a6c413b0e7e175ac
Imed Attia <attia.imed@beja.r-iset.tn>
2022-07-14 01:29:19 +0100

    Simple Vue.js Templete with : Bootstrapping the Project and Build a Consistent and Modern Interface with Kendo UI

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TheNavbar from "./components/TheNavbar.vue";
+import TheDrawer from "./components/TheDrawer.vue";</script>
+
+<template>
+  <TheNavbar />
+  <TheDrawer />
+</template>
